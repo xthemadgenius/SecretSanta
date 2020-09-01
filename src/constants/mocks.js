bb782afef8c1fd929451811f168444f9662bea94
Javier Calderon Jr <javierjcjr@gmail.com>
2020-09-01 01:14:01 -0700

    more progress

---
+++ src/constants/mocks.js
@@ -0,0 +1,44 @@
+const categories = [
+    {
+        id: "games",
+        name: "Games",
+        tags: ["products", "inspirations"],
+        count: 147,
+        image: require("../assets/icons/games.png")
+      },
+      {
+        id: "shoes",
+        name: "Shoes",
+        tags: ["products", "shop"],
+        count: 16,
+        image: require("../assets/icons/shoes.png")
+      },
+      {
+        id: "toys",
+        name: "Toys",
+        tags: ["products", "inspirations"],
+        count: 68,
+        image: require("../assets/icons/toys.png")
+      },
+      {
+        id: "makeup",
+        name: "MakeUp",
+        tags: ["products", "shop"],
+        count: 17,
+        image: require("../assets/icons/makeup.png")
+      },
+      {
+        id: "tech",
+        name: "Technology",
+        tags: ["products", "shop"],
+        count: 47,
+        image: require("../assets/icons/tech.png")
+      },
+      {
+        id: "clothes",
+        name: "Clothes",
+        tags: ["products", "shop"],
+        count: 47,
+        image: require("../assets/icons/clothes.png")
+      }
+]
--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { createAppContainer, createStackNavigator } from "react-navigation";
+import {image} from 'react-native';
+import { createAppContainer, ThemeColors } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
 
 import Welcome from '../screens/Welcome';
 import Browse from '../screens/Browse';
@@ -7,3 +9,31 @@ import Explore from '../screens/Explore';
 import Login from '../screens/Login';
 import Product from '../screens/Product';
 import Settings from '../screens/Settings';
+import SignUp from '../screens/SignUp';
+import Forgot from '../screens/Forgot';
+
+import theme from '../constants';
+
+const screens = createStackNavigator(
+    {
+        Welcome,
+        Login,
+        SignUp,
+        Forgot,
+        Explore,
+        Browse,
+        Product,
+        Settings
+    },
+    {
+        defaultNavigationOptions: {
+            headerStyle:{},
+            headerBackImage: <Image/>,
+            headerBackTitle: null,
+            headerLeftContainerStyle: {},
+            headerRightContainerStyle: {},
+        }
+    }
+);
+
+export default createAppContainer(screens);
