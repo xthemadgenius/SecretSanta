6a783c7357b8c038341db71212903aef4b51e2e6
Javier Calderon Jr <javierjcjr@gmail.com>
2020-08-31 23:05:10 -0700

    more code

---
+++ src/navigation/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { createAppContainer, createStackNavigator } from "react-navigation";
+
+import Welcome from '../screens/Welcome';
+import Browse from '../screens/Browse';
+import Explore from '../screens/Explore';
+import Login from '../screens/Login';
+import Product from '../screens/Product';
+import Settings from '../screens/Settings';
